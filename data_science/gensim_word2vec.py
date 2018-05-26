80e26ae0f581088ac9e8d8f021acb037dea09c9c
beduffy <benfduffy@gmail.com>
2018-05-26 19:10:01 +0100

    Added n-grams, model saving and timing

--- data_science/gensim_word2vec.py
+++ data_science/gensim_word2vec.py
@@ -1,8 +1,10 @@
+import time
 import os
 import sys
 import collections
 
 import gensim
+from gensim.models.phrases import Phrases
 
 data_dir = '../data/txt'
 files = os.listdir(data_dir)
@@ -35,18 +37,38 @@ def calculate_counter(documents):
 
 documents = get_all_documents(1000)
 
-calculate_counter(documents)
+# n-grams
+start = time.time()
+bigram_transformer = Phrases(documents)
+bigram_docs = bigram_transformer[documents]
+trigram_transformer = Phrases(bigram_docs)
+trigram_docs = trigram_transformer[bigram_docs]
 
+corpus = trigram_docs
 
-model = gensim.models.Word2Vec(documents, size=150, window=5, min_count=5, workers=4)
+calculate_counter(corpus)
+print('Time taken to create n-grams: {}'.format(time.time() - start))
 
+# create model
+start = time.time()
+model = gensim.models.Word2Vec(corpus, size=150, window=5, min_count=10, workers=4)
+print('Time taken to create model: {}'.format(time.time() - start))
 
-model.train(documents, total_examples=len(documents), epochs=10)
+start = time.time()
+model.train(corpus, total_examples=len(corpus), epochs=10)
+# todo train per epoch and save model during each epoch
+print('Time taken for training: {}'.format(time.time() - start))
 
-print(model.wv.most_similar(positive='deep'))
+for token in ['deep', 'deep_learning', 'artificial_intelligence']:
+    if token in model.wv.vocab:
+        print(model.wv.most_similar(positive=token))
+    else:
+        print(token, 'not in vocab')
 
 print(model)
-print(model['deep'])
+
+fp = 'models/gensim-word2vec-model-vocab-size-{}.pkl'.format(len(model.wv.vocab))
+model.save(fp)
 
 #model.score(["The fox jumped over a lazy dog".split()])
 
