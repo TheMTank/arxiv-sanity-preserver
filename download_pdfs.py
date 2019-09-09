ccec66989613ea13e63d5a17fd1bf956e3458cb9
beduffy <benfduffy@gmail.com>
2019-09-09 11:26:54 +0100

    final version of download_pdfs, by making sure the pdf doesn't exist, only download those

--- download_pdfs.py
+++ download_pdfs.py
@@ -14,12 +14,12 @@ have = set(os.listdir(Config.pdf_dir)) # get list of all pdfs we already have
 
 numok = 0
 numtot = 0
+how_many_exist_in_elastic = 0
 db = pickle.load(open(Config.db_path, 'rb'))
 for pid,j in db.items():
   
   pdfs = [x['href'] for x in j['links'] if x['type'] == 'application/pdf']
   assert len(pdfs) == 1
-  import pdb;pdb.set_trace()
   pdf_url = pdfs[0] + '.pdf'
   basename = pdf_url.split('/')[-1]
   fname = os.path.join(Config.pdf_dir, basename)
@@ -27,27 +27,24 @@ for pid,j in db.items():
   # try retrieve the pdf
   numtot += 1
   try:
-    # todo if pdf id is not in elastic search
-
     paper_id = pdfs[0].split('/')[-1]
     if exists_in_elastic(paper_id):
-        numok += 1
+      how_many_exist_in_elastic += 1
 
-    # if not basename in have and not exists_in_elastic():
-    #   print('fetching %s into %s' % (pdf_url, fname))
-    #   req = urlopen(pdf_url, None, timeout_secs)
-    #   with open(fname, 'wb') as fp:
-    #       shutil.copyfileobj(req, fp)
-    #   time.sleep(0.05 + random.uniform(0,0.1))
-    # else:
-    #   # todo how to stop downloading everything...
-    #   print('%s exists, skipping' % (fname, ))
-    # numok+=1
+    if not basename in have and not exists_in_elastic(paper_id):
+      print('fetching %s into %s' % (pdf_url, fname))
+      req = urlopen(pdf_url, None, timeout_secs)
+      with open(fname, 'wb') as fp:
+          shutil.copyfileobj(req, fp)
+      time.sleep(0.05 + random.uniform(0,0.1))
+    else:
+      print('%s exists, skipping' % (fname, ))
+    numok+=1
   except Exception as e:
     print('error downloading: ', pdf_url)
     print(e)
   
-  print('%d/%d of %d downloaded ok.' % (numok, numtot, len(db)))
+  print('%d/%d of %d downloaded ok. Num in elastic: %d' % (numok, numtot, len(db), how_many_exist_in_elastic))
   
-print('final number of papers downloaded okay: %d/%d' % (numok, len(db)))
+print('final number of papers downloaded okay: %d/%d. Num in elastic: %d' % (numok, len(db), how_many_exist_in_elastic))
 
